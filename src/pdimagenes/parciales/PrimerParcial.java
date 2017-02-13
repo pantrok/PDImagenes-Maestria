e43ea234554da454d244f2b82e832fc5d624999c
Daniel Sánchez Ruiz <pantrok@gmail.com>
2017-02-12 20:58:56 -0600

    Clase Primer Parcial

---
+++ src/pdimagenes/parciales/PrimerParcial.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdimagenes.parciales;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class PrimerParcial {
+    
+    //TODO: Identificar zona de pupila
+    // Tomar zona mas obscura en histograma, hacer algun tipo de desviacion estandar o promedio para tener algo automatico
+    // Una vez con la zona colorear de otro color
+    // Identificar el centro de la circunferencia y sacar el radio hacia algún borde, checar lo del centroide
+    
+}
